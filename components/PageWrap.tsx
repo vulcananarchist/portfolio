bcbb639c93978de734b6068bcaea8b7f9c339770
vulcananarchist <vkmurthy007@gmail.com>
2026-03-30 19:50:50 -0400

    wide nav on all pages

--- components/PageWrap.tsx
+++ components/PageWrap.tsx
@@ -3,10 +3,18 @@ import Footer from './Footer'
 
 export default function PageWrap({ children }: { children: React.ReactNode }) {
   return (
-    <div className="max-w-[560px] mx-auto px-6">
-      <Header />
-      <main className="pb-16">{children}</main>
-      <Footer />
+    <div className="min-h-screen">
+      {/* Wide header */}
+      <div className="max-w-[1100px] mx-auto px-6">
+        <Header />
+      </div>
+      {/* Centred content */}
+      <div className="max-w-[1100px] mx-auto px-6">
+        <div className="max-w-[560px]">
+          <main className="pb-16">{children}</main>
+          <Footer />
+        </div>
+      </div>
     </div>
   )
 }
