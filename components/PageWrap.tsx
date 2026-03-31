0bed7a9fd2a4597ff8a89e151280ee2312699c8c
vulcananarchist <vkmurthy007@gmail.com>
2026-03-30 20:16:35 -0400

    mobile responsiveness audit fixes

--- components/PageWrap.tsx
+++ components/PageWrap.tsx
@@ -4,12 +4,10 @@ import Footer from './Footer'
 export default function PageWrap({ children }: { children: React.ReactNode }) {
   return (
     <div className="min-h-screen">
-      {/* Wide header */}
-      <div className="max-w-[1100px] mx-auto px-6">
+      <div className="max-w-[1100px] mx-auto px-4 sm:px-6">
         <Header />
       </div>
-      {/* Centred content */}
-      <div className="max-w-[1100px] mx-auto px-6">
+      <div className="max-w-[1100px] mx-auto px-4 sm:px-6">
         <div className="max-w-[560px]">
           <main className="pb-16">{children}</main>
           <Footer />
